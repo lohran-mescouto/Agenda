e8d8008f0d3233bf1e744fed10883aac5d65c266
Diozefe <diozefe.f.morais@gmail.com>
2018-11-06 21:49:46 -0300

    sei la

---
+++ src/java/SalvarContato.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.PreparedStatement;
+import com.mysql.jdbc.Driver;
+/**
+ *
+ * @author aluno
+ */
+@WebServlet(urlPatterns = {"/salvar-contato"})
+public class SalvarContato extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            
+            response.setContentType("text/html");
+            
+            String nome = request.getParameter ("nome");
+            String endereco = request.getParameter ("endereco");
+            
+         
+            DriverManager.registerDriver(new Driver());
+            Connection mysql = DriverManager.getConnection (
+                "jdbc:mysql://localhost/Agenda",
+                "root",
+                "1q");
+        
+        
+            String instrucao = "insert into contatos " + "(nome, endereco)" + " values (?, ?)";
+            
+            PreparedStatement ps = 
+                    mysql.prepareStatement(instrucao);
+            
+            ps.setString(1, nome);
+            ps.setString(2, endereco);
+            ps.execute();
+            
+        } catch (SQLException ex) {
+            out.println(ex.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
